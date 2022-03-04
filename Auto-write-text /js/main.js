1385377f8a243d77d0677e436e7e2d3638c95a77
Din Ho <100463706+hodinsay@users.noreply.github.com>
2022-03-03 22:35:29 -0800

    Add files via upload

---
+++ Auto-write-text /js/main.js
@@ -0,0 +1,16 @@
+const text = 'Hello World! Welcome to OASIS Player #...'
+
+let index = 0;
+
+function writeText() {
+    document.body.innerText = text.slice(0, index);
+
+    index++;
+
+    if (index > text.length) {
+        index = 0;
+    }
+}
+
+setInterval(writeText, 100);
+
